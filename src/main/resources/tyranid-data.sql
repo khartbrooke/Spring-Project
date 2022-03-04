4c5a62a8fc2d934f0dd3ac764f3530776e565b81
Kieran Hart-Brooke <kieranhartbrooke@gmail.com>
2022-03-04 10:13:13 +0000

    fixed sql files

--- src/main/resources/tyranid-data.sql
+++ src/main/resources/tyranid-data.sql
@@ -1,2 +1,2 @@
 insert into tyranid (name, hiveFleet, points) values ('Hive Tyrant', 'Hive Fleet Leviathan', 300);
-insert into tyranid (name, hiveFleet, points) values ('Zoanthrope', 'Hive Fleet Kraken'. 60);
+insert into tyranid (name, hiveFleet, points) values ('Zoanthrope', 'Hive Fleet Kraken', 60);
